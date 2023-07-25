182eacbef630967013eb253bb456c1bda1dba96d
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-25 19:48:59 -0300

    feito o exer6 e concluido o projeto

--- exer6/index.js
+++ exer6/index.js
@@ -1,10 +1,23 @@
-const num = 30
-for(let i = num; i >= 1; i--){
-    document.write(`${i} `)
-        if(num % i === 0){
-            document.write(`[${i}]`)
-        }
+function numeroPrimo(numero) {
+    if (numero <= 1) return false
+    if (numero <= 3) return true
+    
+    for (let i = 2; i <= Math.sqrt(numero); i++) {
+      if (numero % i === 0) return false
+    }
+    
+    return true
 }
+function contagemRegrassivaPrimos() {
+    let saida = ""
+  
+    for (let i = 30; i >= 1; i--) {
+      saida += numeroPrimo(i) ? `[${i}] ` : `${i} `
+    }
+  
+    document.write(saida)
+}
+contagemRegrassivaPrimos()
 
 // 6. Desenvolva um algoritmo que mostre uma contagem regressiva de
 // 30 até 1, marcando os números que forem primos, exatamente
