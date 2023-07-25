324c7063ad159b7d51a0eb9741a4db85f5c0ff13
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-25 19:33:06 -0300

    feito o exer7

--- exer7/index.js
+++ exer7/index.js
@@ -1,4 +1,29 @@
+const quantidadeNumeros = 10
+let soma = 0
+let quantidadePositivos = 0
+let quantidadeNegativos = 0
 
+for (let i = 0; i < quantidadeNumeros; i++) {
+  const numero = parseFloat(prompt(`Digite o número ${i + 1}:`))
+
+  soma += numero
+
+  if (numero > 0) {
+    quantidadePositivos++
+  } else if (numero < 0) {
+    quantidadeNegativos++
+  }
+}
+
+const media = soma / quantidadeNumeros;
+const percentualPositivos = (quantidadePositivos / quantidadeNumeros) * 100
+const percentualNegativos = (quantidadeNegativos / quantidadeNumeros) * 100
+
+document.write(`Média aritmética: ${media.toFixed(2)}.<br>`)
+document.write(`Quantidade de valores positivos: ${quantidadePositivos}.<br>`)
+document.write(`Quantidade de valores negativos: ${quantidadeNegativos}.<br>`)
+document.write(`Percentual de valores positivos: ${percentualPositivos.toFixed(2)}%.<br>`)
+document.write(`Percentual de valores negativos: ${percentualNegativos.toFixed(2)}%.`)
 
 // 7. Desenvolva um algoritmo que leia 10 números, calcule e escreva a
 // média aritmética dos valores lidos, a quantidade de valores
